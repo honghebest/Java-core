0997c6762d991d9aa10dd40bbb1b3c364de61402
hongghe <364930855@qq.com>
2018-07-30 21:08:39 +0800

    process handler.

---
+++ src/main/java/com/hongghe/basicjava/process/asynchrony/PackageInfo.java
@@ -0,0 +1,7 @@
+package com.hongghe.basicjava.process.asynchrony;
+
+/**
+ * 同步线程处理
+ */
+public class PackageInfo {
+}
---
+++ src/main/java/com/hongghe/basicjava/process/synchrony/PackageInfo.java
@@ -0,0 +1,7 @@
+package com.hongghe.basicjava.process.synchrony;
+
+/**
+ * 异步线程处理
+ */
+public class PackageInfo {
+}

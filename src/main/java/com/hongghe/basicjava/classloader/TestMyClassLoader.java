9d38ca71aa5deab2ea8d3f8c43ee7c464a823621
hongghe <364930855@qq.com>
2018-08-02 00:13:41 +0800

    Class loader.

---
+++ src/main/java/com/hongghe/basicjava/classloader/TestMyClassLoader.java
@@ -0,0 +1,18 @@
+package com.hongghe.basicjava.classloader;
+
+/**
+ * 自定义的类测试
+ *
+ * @author hongghe 02/08/2018
+ */
+public class TestMyClassLoader
+{
+    public static void main(String[] args) throws Exception
+    {
+        MyClassLoader mcl = new MyClassLoader();
+        Class<?> c1 = Class.forName("com.hongghe.basicjava.domain.Person", true, mcl);
+        Object obj = c1.newInstance();
+        System.out.println(obj);
+        System.out.println(obj.getClass().getClassLoader());
+    }
+}

e29cc0847c414679d738bce340afa8ee6a8df2ee
hongghe <364930855@qq.com>
2018-08-05 23:35:34 +0800

    Lock of java .

---
+++ src/main/java/com/hongghe/basicjava/guava/PackageInfo.java
@@ -0,0 +1,8 @@
+package com.hongghe.basicjava.guava;
+
+/**
+ * Guava工具集的一些使用
+ *
+ * @author hongghe 05/08/2018
+ */
+
---
+++ src/main/java/com/hongghe/basicjava/guava/StringsDemo.java
@@ -0,0 +1,9 @@
+package com.hongghe.basicjava.guava;
+
+/**
+ * 与字符串相关的
+ *
+ * @author hongghe 05/08/2018
+ */
+public class StringsDemo {
+}

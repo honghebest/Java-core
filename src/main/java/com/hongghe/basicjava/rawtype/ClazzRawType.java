ac86f8a5e923058875cc627510854bad43c286a7
hongghe <364930855@qq.com>
2018-08-23 11:18:48 +0800

    raw type of java class.

--- src/main/java/com/hongghe/basicjava/rawtype/ClazzRawType.java
+++ src/main/java/com/hongghe/basicjava/rawtype/ClazzRawType.java
@@ -6,11 +6,17 @@ import org.slf4j.LoggerFactory;
 /**
  * @author hongghe 2018/8/23
  */
-public class ClazzRawType<E> {
+public class ClazzRawType<E, V> {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ClazzRawType.class);
 
     public E element;
+    public V value;
+
+    public ClazzRawType(E element, V value) {
+        this.element = element;
+        this.value = value;
+    }
 
     public ClazzRawType(E element) {
         this.element = element;
@@ -27,9 +33,10 @@ public class ClazzRawType<E> {
     }
 
     public static void main(String[] args) {
-        ClazzRawType<String> clazzRawType = new ClazzRawType<>("da");
-        ClazzRawType<Integer> integerClazzRawType = new ClazzRawType<>();
-        integerClazzRawType.element = 123;
+        ClazzRawType<String, Object> clazzRawType = new ClazzRawType<>("element value.", "value value.");
+        LOGGER.info("The clazz raw type is " + clazzRawType.value);
+        ClazzRawType<Integer, Object> integerClazzRawType = new ClazzRawType<>(1);
+        integerClazzRawType.setElement(2);
         LOGGER.info("The clazz raw type is " + integerClazzRawType.element);
     }
 }
--- src/main/java/com/hongghe/basicjava/rawtype/MethodRawType.java
+++ src/main/java/com/hongghe/basicjava/rawtype/MethodRawType.java
@@ -1,7 +1,28 @@
 package com.hongghe.basicjava.rawtype;
 
+import com.google.gson.Gson;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * @author hongghe 2018/8/23
  */
-public class MethodRawType {
+public class MethodRawType<T> {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(MethodRawType.class);
+
+    public MethodRawType<T> getMethodType() {
+        LOGGER.info("The raw type of method.");
+        return new MethodRawType<>();
+    }
+
+    public <E> String getMethodValue(E e) {
+        LOGGER.info("The raw type: " + e.toString());
+        return e.toString();
+    }
+
+    public static void main(String[] args) {
+        MethodRawType methodRawType = new MethodRawType();
+        methodRawType.getMethodValue(Gson.class);
+    }
 }

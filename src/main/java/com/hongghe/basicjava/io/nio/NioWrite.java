c433c5d88c3d8dab09dfbbaab8495bbd89eee0cf
hongghe <364930855@qq.com>
2018-08-28 20:26:07 +0800

    Dead lock.

--- src/main/java/com/hongghe/basicjava/io/nio/NioWrite.java
+++ src/main/java/com/hongghe/basicjava/io/nio/NioWrite.java
@@ -12,16 +12,15 @@ import java.nio.channels.FileChannel;
  */
 public class NioWrite {
 
-    private static final byte message[] = { 83, 111, 109, 101, 32,
-            98, 121, 116, 101, 115, 46 };
+    private static final byte[] MESSAGE = { 83, 111, 109, 101, 32, 98, 121, 116, 101, 115, 46 };
 
     public static void main(String[] args) throws IOException {
         FileOutputStream fileOutputStream = new FileOutputStream("\\test.txg");
         FileChannel fileChannel = fileOutputStream.getChannel();
         ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
 
-        for (int i=0; i<message.length; i++) {
-            byteBuffer.put(message[i]);
+        for (int i=0; i<MESSAGE.length; i++) {
+            byteBuffer.put(MESSAGE[i]);
         }
 
         byteBuffer.flip();
---
+++ src/main/java/com/hongghe/basicjava/lock/dead/DeadLock.java
@@ -0,0 +1,26 @@
+package com.hongghe.basicjava.lock.dead;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * 死锁的例子和实现
+ *
+ * @author hongghe 2018/8/27
+ */
+public class DeadLock {
+
+    public static class DeadLockBean {
+        private Lock lock = new ReentrantLock();
+        public void productDeadLock() throws Throwable {
+            lock.lock();
+            try {
+                throw new Throwable("The exception");
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                lock.unlock();
+            }
+        }
+    }
+}

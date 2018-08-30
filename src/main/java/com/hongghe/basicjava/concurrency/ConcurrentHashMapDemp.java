cbfb32e6304653b2596b82c1b6b4830e874f5eca
hongghe <364930855@qq.com>
2018-08-30 23:14:14 +0800

    Md5.

--- src/main/java/com/hongghe/basicjava/concurrency/ConcurrentHashMapDemp.java
+++ src/main/java/com/hongghe/basicjava/concurrency/ConcurrentHashMapDemp.java
@@ -2,6 +2,7 @@ package com.hongghe.basicjava.concurrency;
 
 import com.google.common.collect.ImmutableMap;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicReference;
@@ -13,12 +14,12 @@ import java.util.concurrent.atomic.AtomicReference;
  */
 public class ConcurrentHashMapDemp {
 
-    private final AtomicReference<ImmutableMap<String, String>> atomicReferenceRef =
+    public final AtomicReference<ImmutableMap<String, String>> atomicReferenceRef =
             new AtomicReference<>(ImmutableMap.of());
-
-
-
+    
     public static void main(String[] args) {
+        Map<String, String> hashMap = new HashMap<>();
         Map<String, String> map = new ConcurrentHashMap<>();
+
     }
 }

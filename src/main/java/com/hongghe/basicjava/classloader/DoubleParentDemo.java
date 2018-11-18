0bd1059c323489d51d5f765e40ec52afa4340e6e
hongghe <364930855@qq.com>
2018-11-18 16:11:31 +0800

    The format.

--- src/main/java/com/hongghe/basicjava/classloader/DoubleParentDemo.java
+++ src/main/java/com/hongghe/basicjava/classloader/DoubleParentDemo.java
@@ -11,14 +11,13 @@ import java.util.StringTokenizer;
 public class DoubleParentDemo {
 
 
-    //ExtClassLoader类中获取路径的代码
+    /**ExtClassLoader类中获取路径的代码*/
     private static File[] getExtDirs() {
         //加载<JAVA_HOME>/lib/ext目录中的类库
         String s = System.getProperty("java.ext.dirs");
         File[] dirs;
         if (s != null) {
-            StringTokenizer st =
-                    new StringTokenizer(s, File.pathSeparator);
+            StringTokenizer st = new StringTokenizer(s, File.pathSeparator);
             int count = st.countTokens();
             dirs = new File[count];
             for (int i = 0; i < count; i++) {

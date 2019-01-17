8001744455eef4306ec0b0bf796dd799a9d43b93
hongghe <364930855@qq.com>
2019-01-17 11:33:11 +0800

    Te

---
+++ src/main/java/com/hongghe/basicjava/lambda/BasicLambda.java
@@ -0,0 +1,12 @@
+package com.hongghe.basicjava.lambda;
+
+/**
+ * @author: hongghe @date: 2019-01-17 11:06
+ */
+public class BasicLambda {
+
+    public static void main(String[] args) {
+        Integer x = 0;
+        System.out.println("");
+    }
+}
---
+++ src/main/java/com/hongghe/basicjava/lambda/CoreLambda.java
@@ -0,0 +1,7 @@
+package com.hongghe.basicjava.lambda;
+
+/**
+ * @author: hongghe @date: 2019-01-17 11:07
+ */
+public class CoreLambda {
+}

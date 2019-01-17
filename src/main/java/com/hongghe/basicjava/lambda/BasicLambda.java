df09642960d8020619430e7d0d50cb3b2a859ee1
hongghe <364930855@qq.com>
2019-01-17 14:04:10 +0800

    The setting

--- src/main/java/com/hongghe/basicjava/lambda/BasicLambda.java
+++ src/main/java/com/hongghe/basicjava/lambda/BasicLambda.java
@@ -6,7 +6,20 @@ package com.hongghe.basicjava.lambda;
 public class BasicLambda {
 
     public static void main(String[] args) {
-        Integer x = 0;
-        System.out.println("");
+        final int x = 1;
+        getA<Integer> a = (y) -> System.out.println(String.valueOf(x + y));
+        a.a(1);
+        System.out.println(a);
+        final int num = 1;
+        Converter<Integer, String> s = (param) -> System.out.println(String.valueOf(param + num));
+        s.convert(2);  // 输出结果为 3
+    }
+
+    public interface Converter<T1, T2> {
+        void convert(int i);
+    }
+
+    public interface getA<T> {
+        void a(int a);
     }
 }

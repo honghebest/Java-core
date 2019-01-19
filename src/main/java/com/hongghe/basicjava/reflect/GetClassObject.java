f9ff25226f59f51c51c5eebef12c62202a5e688b
hongghe <364930855@qq.com>
2019-01-19 17:22:56 +0800

    Java basic data class.

--- src/main/java/com/hongghe/basicjava/reflect/GetClassObject.java
+++ src/main/java/com/hongghe/basicjava/reflect/GetClassObject.java
@@ -1,44 +1,38 @@
 package com.hongghe.basicjava.reflect;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 
-import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 /**
- * 获取累对象的方法
+ * 获取类对象的方法
  *
  * @author hongghe 28/07/2018
  */
+@Slf4j
 public class GetClassObject {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(GetClassObject.class);
-
-    public static void main(String[] args) throws IOException, IllegalAccessException,
-            InstantiationException, NoSuchMethodException, InvocationTargetException {
+    public static void main(String[] args) throws IllegalAccessException, InstantiationException, NoSuchMethodException, InvocationTargetException {
         Class<?> classMethod = MethodDemo.class;
         Object methodObject = classMethod.newInstance();
         Method[] methods = classMethod.getMethods();
-
         Method [] declaredMethods = classMethod.getDeclaredMethods();
-
         Method addMethod = classMethod.getMethod("add", int.class, int.class);
-        LOGGER.info("getMethod method");
+        log.info("getMethod method");
         for (Method method : methods) {
-            LOGGER.info("The method is " + method);
+            log.info("The method is " + method);
             if (method.getName().equalsIgnoreCase("addd")) {
-                LOGGER.info("The method name is add is ");
+                log.info("The method name is add is ");
             }
         }
 
-        LOGGER.info("The declared methods");
+        log.info("The declared methods");
         for (Method method : declaredMethods) {
-            LOGGER.info("The methods is " + method);
+            log.info("The methods is " + method);
         }
 
         Object result = addMethod.invoke(methodObject, 1, 4);
-        LOGGER.info("The result is " + result);
+        log.info("The result is " + result);
     }
 }

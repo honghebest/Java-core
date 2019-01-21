525f8f087c355eb3fd9a989144a8189d23fff435
hongghe <364930855@qq.com>
2019-01-21 15:29:50 +0800

    Java basic data class.

--- src/main/java/com/hongghe/basicjava/lambda/ObjectLambda.java
+++ src/main/java/com/hongghe/basicjava/lambda/ObjectLambda.java
@@ -1,8 +1,12 @@
 package com.hongghe.basicjava.lambda;
 
+import com.google.common.base.Functions;
 import com.google.gson.Gson;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 /**
@@ -36,5 +40,19 @@ public class ObjectLambda {
         Map<String,String> maps = animalClassList.stream()
                 .collect(Collectors.toMap(x -> x.getAnimalClazz(), x-> x.getName()));
         System.out.println(new Gson().toJson(maps));
+
+//        Map<String, SmallAnimal> strings = animalClassList.stream()
+//                .filter(x -> x.getAge() > 1)
+//                .collect(Collectors.toMap(SmallAnimal::getClazz, Functions.identity()));
+//
+//        System.out.println(new Gson().toJson(strings));
+
+        Arrays.asList("a", "b", "c")
+                .stream()
+                .map(Function.identity()) // <- This,
+                .map(str -> str)          // <- is the same as this.
+                .collect(Collectors.toMap(
+                        Function.identity(), // <-- And this,
+                        str -> str));        // <-- is the same as this.
     }
 }

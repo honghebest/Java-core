8ec944b034cba63b7e56fc147904024989ab2b50
hongghe <364930855@qq.com>
2019-01-28 16:37:42 +0800

    Http.

--- src/main/java/com/hongghe/basicjava/inherits/MethodUsingMain.java
+++ src/main/java/com/hongghe/basicjava/inherits/MethodUsingMain.java
@@ -1,9 +1,15 @@
 package com.hongghe.basicjava.inherits;
 
+import java.lang.reflect.Method;
+
 /**
  * @author: hongghe @date: 2019-01-28 16:20
  */
 public class MethodUsingMain {
+
+    public static void isMethod(Method method) {
+
+    }
 
     public static void main(String[] args) {
 

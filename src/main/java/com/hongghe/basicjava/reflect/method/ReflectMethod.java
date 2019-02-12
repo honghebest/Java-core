3d1ac379a83bc6040e97800fa5a2cecb9fb1a470
hongghe <364930855@qq.com>
2019-02-12 14:58:19 +0800

    proxy setting.

--- src/main/java/com/hongghe/basicjava/reflect/method/ReflectMethod.java
+++ src/main/java/com/hongghe/basicjava/reflect/method/ReflectMethod.java
@@ -1,6 +1,9 @@
 package com.hongghe.basicjava.reflect.method;
 
+import com.google.gson.Gson;
 import com.hongghe.basicjava.reflect.SuperMethodDemo;
+
+import java.lang.reflect.Method;
 
 /**
  * The class method of reflect.
@@ -11,5 +14,11 @@ public class ReflectMethod {
 
     public static void main(String[] args) {
         Class<?> classObject = SuperMethodDemo.class;
+        try {
+            Method method = classObject.getMethod("SuperMethodDemo()", SuperMethodDemo.class);
+            System.out.println(new Gson().toJson(method));
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
     }
 }

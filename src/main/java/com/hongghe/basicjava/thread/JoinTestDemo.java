51d0346c6c1da41349cfb02f2b7b9cdfc64b202d
hongghe <364930855@qq.com>
2019-02-16 17:30:09 +0800

    proxy setting.

---
+++ src/main/java/com/hongghe/basicjava/thread/JoinTestDemo.java
@@ -0,0 +1,24 @@
+package com.hongghe.basicjava.thread;
+
+/**
+ * @author: hongghe @date: 2019-02-16 16:24
+ */
+public class JoinTestDemo {
+
+    public static void main(String[] args) {
+        String threadName = Thread.currentThread().getName();
+        System.out.println(threadName + " start.");
+        ThreadJoin t1 = new ThreadJoin();
+        CustomThread t = new CustomThread(t1);
+        try {
+            t1.start();
+            Thread.sleep(2000);
+            t.start();
+            t.join();
+        } catch (Exception e) {
+            System.out.println("Exception from main");
+        }
+        System.out.println(threadName + " end!");
+    }
+}
+
--- src/main/java/com/hongghe/basicjava/thread/ThreadJoin.java
+++ src/main/java/com/hongghe/basicjava/thread/ThreadJoin.java
@@ -36,22 +36,3 @@ class CustomThread extends Thread {
         }
     }
 }
-
-public class JoinTestDemo {
-
-    public static void main(String[] args) {
-        String threadName = Thread.currentThread().getName();
-        System.out.println(threadName + " start.");
-        ThreadJoin t1 = new ThreadJoin();
-        CustomThread t = new CustomThread(t1);
-        try {
-            t1.start();
-            Thread.sleep(2000);
-            t.start();
-            t.join(); //在代碼2里，將此處注釋掉
-        } catch (Exception e) {
-            System.out.println("Exception from main");
-        }
-        System.out.println(threadName + " end!");
-    }
-}

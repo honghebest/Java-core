9f6f170063db7313ff4991cccea7222012e05785
hongghe <364930855@qq.com>
2019-02-18 10:04:43 +0800

    proxy setting.

--- src/main/java/com/hongghe/basicjava/thread/ThreadNotify.java
+++ src/main/java/com/hongghe/basicjava/thread/ThreadNotify.java
@@ -8,4 +8,23 @@ public class ThreadNotify {
     public static void main(String[] args) {
 
     }
+
+    class Notify implements Runnable {
+
+        /**
+         * When an object implementing interface <code>Runnable</code> is used
+         * to create a thread, starting the thread causes the object's
+         * <code>run</code> method to be called in that separately executing
+         * thread.
+         * <p>
+         * The general contract of the method <code>run</code> is that it may
+         * take any action whatsoever.
+         *
+         * @see Thread#run()
+         */
+        @Override
+        public void run() {
+
+        }
+    }
 }

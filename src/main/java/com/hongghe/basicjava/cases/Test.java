a248ca9a361dcf25795daaa077d29408f6272f90
hongghe <364930855@qq.com>
2019-03-15 15:37:42 +0800

    修改代码的格式。

--- src/main/java/com/hongghe/basicjava/cases/Test.java
+++ src/main/java/com/hongghe/basicjava/cases/Test.java
@@ -2,6 +2,7 @@ package com.hongghe.basicjava.cases;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * @author: hongghe @date: 2019-03-15 10:10
@@ -19,7 +20,18 @@ public class Test {
         return simpleDateFormat.parse(time).getTime()/1000;
     }
 
+    private static String FORMATDATE(String time) {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        try {
+            simpleDateFormat.setLenient(false);
+            simpleDateFormat.parse(time);
+        } catch (ParseException e) {
+            return simpleDateFormat.format(new Date(Long.valueOf(time) * 1000));
+        }
+        return time;
+    }
+
     public static void main(String[] args) throws ParseException {
-        System.out.println(isValidDate("1234567899"));
+        System.out.println(FORMATDATE("2016-08-04 10:34:42"));
     }
 }

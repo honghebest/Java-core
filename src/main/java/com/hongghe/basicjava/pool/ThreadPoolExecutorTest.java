882bb197771abfe80d992753284c32699dbb80f1
hongghe <364930855@qq.com>
2019-03-22 12:09:10 +0800

    修改线程。

--- src/main/java/com/hongghe/basicjava/pool/ThreadPoolExecutorTest.java
+++ src/main/java/com/hongghe/basicjava/pool/ThreadPoolExecutorTest.java
@@ -13,14 +13,19 @@ public class ThreadPoolExecutorTest {
     private static int queueDeep = 4;
 
     public void createThreadPool() {
+
         /*
          * 创建线程池，最小线程数为2，最大线程数为4，线程池维护线程的空闲时间为3秒，
          * 使用队列深度为4的有界队列，如果执行程序尚未关闭，则位于工作队列头部的任务将被删除，
          * 然后重试执行程序（如果再次失败，则重复此过程），里面已经根据队列深度对任务加载进行了控制。
          */
-        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(2, 4, 3,
-                TimeUnit.SECONDS, new ArrayBlockingQueue<Runnable>(queueDeep),
+        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(2,
+                4,
+                3,
+                TimeUnit.SECONDS,
+                new ArrayBlockingQueue<>(queueDeep),
                 new ThreadPoolExecutor.DiscardOldestPolicy());
+
         // 向线程池中添加 10 个任务
         for (int i = 0; i < 10; i++) {
             try {
@@ -45,6 +50,7 @@ public class ThreadPoolExecutorTest {
     }
 
     private synchronized int getQueueSize(Queue queue) {
+        System.out.println(queue.size());
         return queue.size();
     }
 

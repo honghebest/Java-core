e014b2a99ef6d02bdab8b9c99f03bdaa7eec3e37
henry <zhanghongheman@gmail.com>
2019-04-09 22:40:02 +0800

    修改线程。

--- src/main/java/com/hongghe/basicjava/TestInteger.java
+++ src/main/java/com/hongghe/basicjava/TestInteger.java
@@ -1,19 +1,32 @@
 package com.hongghe.basicjava;
 
+import com.google.gson.Gson;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author: hongghe @date: 2019-04-08 13:59
  */
 public class TestInteger {
 
 
-    public static void change(Integer i) {
+    public void change(Integer i) {
         i = 2;
+        System.out.println(i);
     }
 
     public static void main(String[] args) {
+        TestInteger testInteger = new TestInteger();
         Integer i = 1;
         System.out.println(i.hashCode());
-        change(i);
+        testInteger.change(i);
         System.out.println(i.hashCode());
+
+        List<Bird> list = new ArrayList<>();
+        Bird bird = new Bird("name", 1);
+        list.add(bird);
+        bird.setAge(2);
+        System.out.println(new Gson().toJson(list));
     }
 }

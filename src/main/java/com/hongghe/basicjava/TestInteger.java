0ad4d8b84c93b55ffb2a4acbcb8b42baff47164c
honghe <zhanghongheman@gmail.com>
2020-06-21 17:06:04 +0800

    修改注释。

--- src/main/java/com/hongghe/basicjava/TestInteger.java
+++ src/main/java/com/hongghe/basicjava/TestInteger.java
@@ -1,12 +1,8 @@
 package com.hongghe.basicjava;
 
-import com.google.gson.Gson;
-
-import java.util.ArrayList;
-import java.util.List;
-
 /**
- * @author: hongghe @date: 2019-04-08 13:59
+ * @author: hongghe
+ * @date: 2019-04-08 13:59
  */
 public class TestInteger {
 
@@ -17,16 +13,8 @@ public class TestInteger {
     }
 
     public static void main(String[] args) {
-        TestInteger testInteger = new TestInteger();
-        Integer i = 1;
-        System.out.println(i.hashCode());
-        testInteger.change(i);
-        System.out.println(i.hashCode());
 
-        List<Bird> list = new ArrayList<>();
-        Bird bird = new Bird("name", 1);
-        list.add(bird);
-        bird.setAge(2);
-        System.out.println(new Gson().toJson(list));
+        Integer num = 12123123;
+        System.out.println(num.hashCode());
     }
 }
